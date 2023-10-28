a99663592a067f84eac7ec07e57d5193cbba25e7
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-28 19:23:11 +0300

    pp

--- prime/Program.cs
+++ prime/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int []a=new int[4];
+
+for(int i=0;i<a.Length;i++){
+	Console.Write("Enter Number: ");
+	a[i]=int.Parse(Console.ReadLine());
+	}
+
+int size=0;
+
+foreach(int i in a){
+	for(int j=1;j<int.Parse(Math.Sqrt(i));j++){
+		bool isPrime=true;
+		if(){
+			
+		}
+	}
+	}
