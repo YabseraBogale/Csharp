124ec0a82debac47483c83dd507014275d8943bb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-31 19:56:21 +0300

    linq is good for database filter and err

--- linq/Program.cs
+++ linq/Program.cs
@@ -3,7 +3,7 @@
 int[] number={1,2,5,10};
 
 var subset=from i in number where i%2==0 select i;
-
+// had to use foreach to sove it
 foreach(var i in subset){
 	Console.WriteLine($"subset: {i}");
 	}
