9342a6a56a2fab7220de48df11247e01932947b1
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-31 16:02:28 +0300

    Linq and where array

--- linq/Program.cs
+++ linq/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] number={1,2,5,10};
+
+var subset=from i in number where i%2==0 select i;
+
+foreach(var i in subset){
+	Console.WriteLine($"subset: {i}");
+	}
