e7c851b6947ecdf2c5b918a6df5ef13a066bf960
YabseraBogale <yabserapython@gmail.com>
2024-08-08 14:01:31 +0300

    ok

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d34a30766d9b44e23f2ed06b0e6af5bc9b05686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6857f8884399774b51e60cedff567accd1ebb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
